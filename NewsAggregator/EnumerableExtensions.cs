5dc34ec35a0f5403613c439719767b0dd39ac08a
Pierre GILLON <contact@pierregillon.com>
2020-02-23 16:21:49 +0100

    Change Chunk implementation to avoid multiple enumeration

--- NewsAggregator/EnumerableExtensions.cs
+++ NewsAggregator/EnumerableExtensions.cs
@@ -5,12 +5,19 @@ namespace NewsAggregator
 {
     public static class EnumerableExtensions
     {
-        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int chunkSize)
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> values, int chunkSize)
         {
-            while (source.Any()) {
-                yield return source.Take(chunkSize);
-                source = source.Skip(chunkSize);
+            using var enumerator = values.GetEnumerator();
+            while (enumerator.MoveNext()) {
+                yield return GetChunk(enumerator, chunkSize).ToList();
             }
+        }
+
+        private static IEnumerable<T> GetChunk<T>(IEnumerator<T> enumerator, int chunkSize)
+        {
+            do {
+                yield return enumerator.Current;
+            } while (--chunkSize > 0 && enumerator.MoveNext());
         }
     }
 }
