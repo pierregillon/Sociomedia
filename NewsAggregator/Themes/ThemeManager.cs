48eb8c8e8f59364feaa29b488effc7d5ba86cbcd
Pierre GILLON <contact@pierregillon.com>
2020-02-25 20:28:59 +0100

    Code cleaning

--- NewsAggregator/Themes/ThemeManager.cs
+++ NewsAggregator/Themes/ThemeManager.cs
@@ -8,7 +8,6 @@ namespace NewsAggregator.Themes
     {
         private readonly List<ThemeArticle> _articles = new List<ThemeArticle>();
         private readonly List<Theme> _themes = new List<Theme>();
-        private readonly Dictionary<ThemeArticle, List<ThemeArticle>> _comparison = new Dictionary<ThemeArticle, List<ThemeArticle>>();
         private readonly List<IDomainEvent> _uncommittedEvents = new List<IDomainEvent>();
 
         public IReadOnlyCollection<IDomainEvent> UncommittedEvents => _uncommittedEvents;
@@ -16,25 +15,17 @@ namespace NewsAggregator.Themes
         public void Add(ThemeArticle article)
         {
             foreach (var existingArticle in _articles) {
-                if (existingArticle == article || _comparison.ContainsKey(article) && _comparison[article]?.Contains(existingArticle) == true) {
+                var keywordIntersection = existingArticle.Keywords.Intersect(article.Keywords).ToArray();
+                if (!keywordIntersection.Any()) {
                     continue;
                 }
-                var keywordIntersection = existingArticle.Keywords.Intersect(article.Keywords).ToArray();
-                if (keywordIntersection.Any()) {
-                    var existing = _themes.FirstOrDefault(theme => keywordIntersection.All(keyword => theme.Keywords.Contains(keyword)));
-                    if (existing == null) {
-                        Apply(new NewThemeCreated(Guid.NewGuid(), keywordIntersection, new[] { existingArticle, article }));
-                    }
-                    else {
-                        if (!existing.Contains(article)) {
-                            Apply(new ArticleAddedToTheme(existing.Id, article));
-                        }
-                    }
+                var existingTheme = _themes.FirstOrDefault(theme => keywordIntersection.All(keyword => theme.Keywords.Contains(keyword)));
+                if (existingTheme == null) {
+                    Apply(new NewThemeCreated(Guid.NewGuid(), keywordIntersection, new[] { existingArticle, article }));
                 }
-                if (_comparison.ContainsKey(existingArticle) == false) {
-                    _comparison.Add(existingArticle, new List<ThemeArticle>());
+                else if (!existingTheme.Contains(article)) {
+                    Apply(new ArticleAddedToTheme(existingTheme.Id, article));
                 }
-                _comparison[existingArticle].Add(article);
             }
             _articles.Add(article);
         }
