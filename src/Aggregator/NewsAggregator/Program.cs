f18b12d3da6dbf1c15d9bc01b9eec865255278c8
Pierre GILLON <contact@pierregillon.com>
2020-05-04 18:38:42 +0200

    Add FranceTVInfo source

--- src/Aggregator/NewsAggregator/Program.cs
+++ src/Aggregator/NewsAggregator/Program.cs
@@ -25,11 +25,10 @@ namespace NewsAggregator
             var commandDispatcher = container.GetInstance<ICommandDispatcher>();
 
             await commandDispatcher.Dispatch(new AddRssSourceCommand(new Uri("https://www.lemonde.fr/rss/une.xml")));
-            await commandDispatcher.Dispatch(new AddRssSourceCommand(new Uri("https://www.lemonde.fr/international/rss_full.xml")));
-            await commandDispatcher.Dispatch(new AddRssSourceCommand(new Uri("https://www.lemonde.fr/economie/rss_full.xml")));
             await commandDispatcher.Dispatch(new AddRssSourceCommand(new Uri("https://www.marianne.net/rss_marianne.xml")));
             await commandDispatcher.Dispatch(new AddRssSourceCommand(new Uri("http://rss.liberation.fr/rss/9/")));
-
+            await commandDispatcher.Dispatch(new AddRssSourceCommand(new Uri("https://www.francetvinfo.fr/france.rss")));
+            
             await commandDispatcher.Dispatch(new SynchronizeRssSourcesCommand());
             await commandDispatcher.Dispatch(new SynchronizeRssSourcesCommand());
 
